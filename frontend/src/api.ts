ee8eb2bd5114f190941f41630a94b73119d46c13
ShinobUu22 <141315924+ShinobUu22@users.noreply.github.com>
2024-06-23 14:21:14 +0545

    Update api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -6,7 +6,9 @@ export const searchRecipes = async (searchTerm:string, page:number) => {
     baseUrl.searchParams.append("searchTerm",searchTerm)
     baseUrl.searchParams.append("page",String(page))
 
-    const response = await fetch(baseUrl)
+    const response = await fetch(baseUrl, {
+  mode: 'no-cors'
+})
     if(!response.ok){
         throw new Error(`HTTP err! Status: ${response.status}`)
     }
@@ -15,7 +17,9 @@ export const searchRecipes = async (searchTerm:string, page:number) => {
 
 export const getRecipeSummary=async (recipeId:string)=>{
     const url = new URL(`https://recipe-app-api-drab.vercel.app/api/recipes/${recipeId}/summary`)
-    const response = await fetch(url)
+    const response = await fetch(url, {
+  mode: 'no-cors'
+})
     if(!response.ok){
         throw new Error(`HTTP err! Status: ${response.status}`)
     }
@@ -24,7 +28,9 @@ export const getRecipeSummary=async (recipeId:string)=>{
 
 export const getFavourites=async ()=>{
     const url =new URL(`https://recipe-app-api-drab.vercel.app/api/recipes/favourite`)
-    const response = await fetch(url)
+    const response = await fetch(url, {
+  mode: 'no-cors'
+})
     if(!response.ok){
         throw new Error(`Http err! Status: ${response.status}`);
         
@@ -42,6 +48,7 @@ export const addFav=async(recipe:Recipe)=>{
         headers:{
             "Content-Type":"application/json"
         },
+         mode: 'no-cors',
         body:JSON.stringify(body)
     })
 
@@ -61,6 +68,7 @@ export const delFav=async(recipe:Recipe)=>{
         headers:{
             "Content-Type":"application/json"
         },
+        mode: 'no-cors',
         body:JSON.stringify(body)
     })
 
