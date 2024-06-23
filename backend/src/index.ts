8f39e9ab7a4ff06565cdc9ea70250fa666537a8f
ShinobUu22 <141315924+ShinobUu22@users.noreply.github.com>
2024-06-23 14:04:15 +0545

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin:["https://recipe-app-frontend-sage.vercel.app/"],
+        origin:["https://recipe-app-frontend-sage.vercel.app"],
         methods:["POST","GET","DELETE"],
         credentials:true
     }
